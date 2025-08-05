24efc738ccae9a733d34e3851adf0ca633e2db55
Sachin Verma <sacvik23@gmail.com>
2025-08-05 22:14:57 +0530

    AWS production deployment script for a Python application using Docker and AWS ECR.

--- AWSDeoply.sh
+++ AWSDeoply.sh
@@ -1,7 +1,20 @@
+## Tagging docker image with AWS account
+
+#!/bin/bash
+set -ex
+
+# Variables - replace with your actual values
+REPO_NAME=model_prediction
+AWS_ACCOUNT_ID="<account id here>"  # Replace with your AWS account ID
+REGION=us-west-2 # Replace with your desired AWS region
+
 ## Authenticate Docker to ECR
-aws ecr get-login-password --region <region> | \
-    docker login --username AWS --password-stdin <aws_account_id>.dkr.ecr.<region>.amazonaws.com
+aws ecr get-login-password --region $REGION | \
+    docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
 ## push image to AWS
-docker push <aws_account_id>.dkr.ecr.<region>.amazonaws.com/<your-repo-name>:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:latest
+
+
+echo "Docker image pushed to AWS!"
 
--- build_docker_with_AWS_tag.sh
+++ build_docker_with_AWS_tag.sh
@@ -1,4 +1,17 @@
 ## Tagging docker image with AWS account
 
-docker build -t <your-repo-name> .
-docker tag <your-repo-name>:latest <aws_account_id>.dkr.ecr.<region>.amazonaws.com/<your-repo-name>:latest
+#!/bin/bash
+set -ex
+
+# Variables - replace with your actual values
+REPO_NAME=model_prediction
+AWS_ACCOUNT_ID="<account id here>"  # Replace with your AWS account ID
+REGION=us-west-2
+
+# Build the Docker image
+docker build -t $REPO_NAME .
+
+# Tag the Docker image
+docker tag $REPO_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:latest
+
+echo "Docker image built and tagged successfully!"
